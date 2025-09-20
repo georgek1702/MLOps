c20360a37f419626c2e52752371992915934af58
georgek1702 <108771446+georgek1702@users.noreply.github.com>
2025-09-20 18:04:41 +0530

    Update prep.py

--- Tourism_Project/model_building/prep.py
+++ Tourism_Project/model_building/prep.py
@@ -10,7 +10,7 @@ from huggingface_hub import login, HfApi
 
 # Define constants for the dataset and output paths
 api = HfApi(token=os.getenv("HF_TOKEN"))
-DATASET_PATH = "hf://datasets/Georgek17/CustomerVisitPredictor/tourism.csv"
+DATASET_PATH = "hf://datasets/Georgek17/CustomerVisitPredictor.csv"
 tourism_dataset = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
 
